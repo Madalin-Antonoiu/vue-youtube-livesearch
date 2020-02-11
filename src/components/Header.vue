6ea2b22cce381775fc802bbf3c2648725f5ad397
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-11 17:57:43 +0200

    INIT. Works.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,29 @@
 <template>
-  <div class="header">
-     <img alt="Vue logo" src="../assets/logo.png" style="width:64px; height:64px;float:left; ">
-     <h1 style="float:left;">Youtube Livesearch</h1>
-   </div>
+  <header>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-5">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <i class="fab fa-youtube fa-2x"></i>
+          <span class="ml-3">YouTube Search</span>
+        </a>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'Header',
-  props: {
-    msg: String
-  }
-}
+  name: 'Header'
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.header{
-  display:inline-block;
-  margin: 0 auto;
+i {
+  vertical-align: middle;
+  color: red;
 }
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+i + span {
+  vertical-align: middle;
 }
 </style>

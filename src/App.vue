32796f93db81e1264667f019fcd7811bdb617442
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-02-11 20:24:29 +0200

    Made it bit more modular, ready for importing into mother-app

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,18 @@
 <template>
   <div id="app">
-    <Header/>
-    <SearchForm v-on:search="search"/>
-    <SearchResults
-      v-if="videos.length > 0"
-      v-bind:videos="videos"
-      v-bind:reformattedSearchString="reformattedSearchString"
-    />
-    <Pagination
-      v-if="videos.length > 0"
-      v-bind:prevPageToken="api.prevPageToken"
-      v-bind:nextPageToken="api.nextPageToken"
-      v-on:prev-page="prevPage"
-      v-on:next-page="nextPage"
-    />
+    <YoutubeSearch/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header';
-import SearchForm from './components/SearchForm';
-import SearchResults from './components/SearchResults';
-import Pagination from './components/Pagination';
-import axios from 'axios';
+import YoutubeSearch from './components/YoutubeSearch';
 
 export default {
   name: 'app',
   components: {
-    Header,
-    SearchForm,
-    SearchResults,
-    Pagination
-  },
-  data() {
-    return {
-      videos: [],
-      reformattedSearchString: '',
-      api: {
-        baseUrl: 'https://www.googleapis.com/youtube/v3/search?',
-        part: 'snippet',//need to add statistics
-        type: 'video',
-        order: 'viewCount',
-        maxResults: 12,
-        q: '',
-        key: 'AIzaSyCL21U-qh0NwQhH2I7WS9rinqpy8Oi3ZOE', //YOUR_API_KEY. Private Repo - Safe
-        prevPageToken: '',
-        nextPageToken: ''
-      }
-    };
-  },
-  methods: {
-    search(searchParams) {
-      this.reformattedSearchString = searchParams.join(' ');
-      this.api.q = searchParams.join('+');
-      const { baseUrl, part, type, order, maxResults, q, key } = this.api;
-      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}`;
-      this.getData(apiUrl);
-    },
-
-    prevPage() {
-      const { baseUrl, part, type, order, maxResults, q, key, prevPageToken } = this.api;
-      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}&pageToken=${prevPageToken}`;
-      this.getData(apiUrl);
-    },
-
-    nextPage() {
-      const { baseUrl, part, type, order, maxResults, q, key, nextPageToken } = this.api;
-      const apiUrl = `${baseUrl}part=${part}&type=${type}&order=${order}&q=${q}&maxResults=${maxResults}&key=${key}&pageToken=${nextPageToken}`;
-      this.getData(apiUrl);
-    },
-
-    getData(apiUrl) {
-      axios
-        .get(apiUrl)
-        .then(res => {
-          this.videos = res.data.items;
-          this.api.prevPageToken = res.data.prevPageToken;
-          this.api.nextPageToken = res.data.nextPageToken;
-        })
-        .catch(error => console.log(error));
-    }
+    YoutubeSearch
   }
-};
+}
 </script>
 
 <style lang="css">
@@ -90,51 +21,6 @@ export default {
     max-width:550px;
     margin:0 auto;
   }
-  .card-text{
-    color: rgb(96, 96, 96);
-    margin:10px;
-    line-height:18px;
-      font-weight:400;
-    font-family:Roboto, Arial, sans-serif;
-    font-size:13px;
-  }
-  .card-title{
-    font-size: 18px;
-    margin:0px 10px 0px 10px;
-    color:rgb(13, 13, 13);
-    font-weight:400;
-    font-family:Roboto, Arial, sans-serif;
-  }
-  .card-subtitle{
-      margin:0px 10px 0px 10px;
-    font-size:13px;
-    color:rgb(96, 96, 96);
-    font-family:Roboto, Arial, sans-serif;
-    font-weight:400;
-  }
+ 
 
-  .card-thumbnail{
-    float: left;
-    width: 246px;
-  }
-
-  .card-body{
-     float: left;
-      width: calc(100% - 246px);
-    /* max-width:374px;
-    width:100%; */
-    /* margin-bottom:16px; */
-      height:142px;
-      overflow:hidden;
-      
-  }
-  #card-contents{
-    margin-bottom:16px;
-  }
-
-  #card-contents:after {
-  content: "";
-  display: table;
-  clear: both;
-}
 </style>
